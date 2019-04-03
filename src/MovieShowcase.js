278b3cdda5c22c8724da4076b38d122808596a49
rudyNK <rudy@noukod.com>
2019-04-03 10:04:38 -0400

    update all

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,7 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return the correct 
+    return movieData.map((data, index) => <MovieCard key={index} title={data.title} IMDBRating={data.IMDBRating} genres={data.genres} poster={data.poster}/>);
   }
 
   render() {
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,16 +12,23 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    const rating = this.props.IMDBRating;
+    if (rating === null) {
+      return <h4>no rating found</h4>;
+    }
+    else {
+      return <img src={imgMapper[rating]} alt="" />
+    }
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement()}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
     )
   }
